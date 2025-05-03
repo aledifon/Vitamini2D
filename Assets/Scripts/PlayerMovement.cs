c8d321e718a28c6cbbc18b9985971541fd89223f
aledifon <aledifon89@gmail.com>
2025-05-03 12:44:21 +0200

    Updated Vitamini Project to New Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,56 +13,55 @@ public class PlayerMovement : MonoBehaviour
     [Header("Jump")]
     [SerializeField] float jumpForce;       // Jumping applied Force
     [SerializeField] bool jumpPressed;       // Jumping applied Force
+    
+    // UI
+    [Header("Acorn")]
+    private float numAcorn;
+    [SerializeField] private TextMeshProUGUI textAcornUI;
+
+    [Header("Elevator")]
+    [SerializeField] LayerMask elevatorLayer;
 
     [Header("Raycast")]
     [SerializeField] Transform groundCheck;  //Raycast origin point (Vitamini feet)
     [SerializeField] LayerMask groundLayer;  //Ground Layer
     [SerializeField] float rayLength;       //Raycast Length
-    [SerializeField] bool isGrounded;       //Ground touching flag
-
-    // UI
-    private float acornCounter;
-    [SerializeField] private TextMeshProUGUI acornCounterTextUI;
-
-    // Movement vars.
-    float horizontal;
-    Vector2 inputPlayerVelocity;    // Velocity given by the player's input
-    Vector2 targetVelocity;          // Desired target player Speed(Velocity Movement type through rb2D)
-    Vector2 dampVelocity;            // Player's current speed storage (Velocity Movement type through r
-
+    [SerializeField] bool isGrounded;       //Ground touching flag    
+       
     // GO Components
     Rigidbody2D rb2D;
+    // Movement vars.
+    float horizontal;
+    //Vector2 inputPlayerVelocity;    // Velocity given by the player's input
+    Vector2 targetVelocity;          // Desired target player Speed(Velocity Movement type through rb2D)
+    Vector2 dampVelocity;            // Player's current speed storage (Velocity Movement type through r
+    Vector2 direction;              // To handle the direction with the New Input System
+
+    // GO Components
     SpriteRenderer spriteRenderer;
     Animator animator;
 
+    #region Unity API
     void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();   
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
 
-        acornCounter = 0;
-        acornCounterTextUI.text = acornCounter.ToString();
+        numAcorn = 0;
+        textAcornUI.text = numAcorn.ToString();
     }
     private void Update()
-    {
-        InputPlayer();
-
-        // Launch raycast
-        RaycastGrounded();
-
-        // Update the target Velocity
-        targetVelocity = new Vector2(horizontal * speed, rb2D.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            jumpPressed = true;
-
+    {               
         // Update the player's gravity when falling down
         ChangeGravity();
-        // Flip the player sprite
-        FlipSprite(horizontal);
-        // Change the animations State
-        Animating(horizontal);
+        // Launch the raycast to detect the ground
+        RaycastGrounded();
+        // Controls if the player is on the elevator
+        //Elevator();
+
+        // Jumping Animation
+        AnimatingJumpìng();        
 
         // Update the Input player velocity        
         //inputPlayerVelocity = new Vector2(horizontal * speed, 0);
@@ -76,55 +74,6 @@ public class PlayerMovement : MonoBehaviour
         if (jumpPressed)
             Jump();        
     }
-    void InputPlayer()
-    {
-        horizontal = Input.GetAxis("Horizontal");
-    }
-    void RaycastGrounded()
-    {
-        // Raycast Launching
-        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down,rayLength, groundLayer);
-        // Raycast Debugging
-        Debug.DrawRay(groundCheck.position,Vector2.down*rayLength,Color.red);
-    }
-    // Player jump handling
-    void Jump()
-    {
-        jumpPressed = false;
-        rb2D.AddForce(Vector2.up*jumpForce);
-    }
-    private void ChangeGravity()
-    {
-        // Gravity will be heavier when the player is falling down
-        if(rb2D.velocity.y<0)
-            rb2D.gravityScale = 2.5f;
-        else
-            rb2D.gravityScale = 1;
-    }
-    void UpdateMovement()
-    {                
-        // Applies the correspondent new velocity        
-        rb2D.velocity = Vector2.SmoothDamp(rb2D.velocity, targetVelocity, ref dampVelocity, smoothTime);
-
-        // Applies the correspondent new velocity        
-        //rb2D.velocity = Vector2.SmoothDamp(rb2D.velocity, targetVelocity, ref dampVelocity, smoothTime);
-    }
-
-    // Flip the Player sprite in function of its movement
-    void FlipSprite(float horizontal)
-    {
-        if (horizontal < 0 && rb2D.velocity.x < 0)
-            spriteRenderer.flipX = true;
-        else if (horizontal > 0 && rb2D.velocity.x > 0)
-            spriteRenderer.flipX = false;
-    }
-    private void Animating(float horizontal)
-    {        
-        animator.SetBool("IsRunning", horizontal != 0);
-
-        animator.SetBool("IsJumping",!isGrounded);
-    }
-
     // Collisions
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -133,9 +82,82 @@ public class PlayerMovement : MonoBehaviour
             // Acorn dissappear
             Destroy(collision.collider.gameObject);
             // Increase Acorn counter
-            acornCounter++;
+            numAcorn++;
             // Update Acorn counter UI Text
-            acornCounterTextUI.text = acornCounter.ToString();
+            textAcornUI.text = numAcorn.ToString();
         }
     }
+    #endregion
+
+    #region Raycast
+    void RaycastGrounded()
+    {
+        // Raycast Launching
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down,rayLength, groundLayer);
+        // Raycast Debugging
+        Debug.DrawRay(groundCheck.position,Vector2.down*rayLength,Color.red);
+    }
+    #endregion
+
+    #region Movement
+    public void JumpActionInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && isGrounded)
+        {
+            jumpPressed = true;
+        }        
+    }
+    public void MoveActionInput(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();        
+        targetVelocity = new Vector2(direction.x * speed, rb2D.velocity.y);
+        // Flip the player sprite & change the animations State
+        FlipSprite(direction.x);       
+        AnimatingRunning(direction.x);
+    }
+    // Player jump handling
+    void Jump()
+    {
+        jumpPressed = false;
+        rb2D.AddForce(Vector2.up*jumpForce);
+    }    
+    void UpdateMovement()
+    {                
+        // Applies the correspondent new velocity        
+        rb2D.velocity = Vector2.SmoothDamp(rb2D.velocity, targetVelocity, ref dampVelocity, smoothTime);
+    }
+    private void ChangeGravity()
+    {
+        // Gravity will be heavier when the player is falling down
+        if (rb2D.velocity.y < 0)
+            rb2D.gravityScale = 2.5f;
+        else
+            rb2D.gravityScale = 1;
+    }
+    #endregion    
+
+    #region Sprite & Animations
+    // Flip the Player sprite in function of its movement
+    void FlipSprite(float horizontal)
+    {
+        if (horizontal < 0 && rb2D.velocity.x < 0)
+            spriteRenderer.flipX = true;
+        else if (horizontal > 0 && rb2D.velocity.x > 0)
+            spriteRenderer.flipX = false;
+    }
+    private void AnimatingRunning(float horizontal)
+    {
+        animator.SetBool("IsRunning", horizontal != 0);
+    }
+    private void AnimatingJumpìng()
+    {
+        animator.SetBool("IsJumping", !isGrounded);
+    }
+    #endregion    
+
+    // Old Input System
+    //void InputPlayer()
+    //{
+    //    horizontal = Input.GetAxis("Horizontal");
+    //}    
 }
